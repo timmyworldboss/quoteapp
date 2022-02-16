3bbea76f6dcec5a358a06d9d3dd88057de429c32
timmy <timothy.ndung'u@student.moringaschool.com>
2022-02-16 10:12:30 +0300

    quotes ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'welcome to tech quotes';
   quotes:Quote[] = [
-    new Quote (1,'Charles','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
-    new Quote (2,'Wendy','Technology…','Technology… the knack of so arranging  that we don’t have to experience it.','Max Frisch',new Date(2019,7,14),0,0),
+    new Quote (1,'Tommy','Experience','What Does Not Kill you makes you Stronger.','gregory issacs', new Date(2019,7,12),0,0),
+    new Quote (2,'Mary Jane','mindsate…','Madness is like gravity all it needs is a little Push.','The joker',new Date(2019,7,14),0,0),
     new Quote (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2019,6,3),0,0),
     new Quote (4,'Beatrice','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2019,7,7),0,0),
     new Quote (5,'Charles','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2019,7,17),0,0),
