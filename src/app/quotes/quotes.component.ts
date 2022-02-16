7c3c655a9093f0724b650820a52d126be3ccb841
timmy <timothy.ndung'u@student.moringaschool.com>
2022-02-16 10:16:46 +0300

    quote dates

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'welcome to tech quotes';
   quotes:Quote[] = [
-    new Quote (1,'Tommy','Experience','What Does Not Kill you makes you Stronger.','gregory issacs', new Date(2019,7,12),0,0),
-    new Quote (2,'Mary Jane','mindsate…','Madness is like gravity all it needs is a little Push.','The joker',new Date(2019,7,14),0,0),
-    new Quote (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2019,6,3),0,0),
-    new Quote (4,'Beatrice','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2019,7,7),0,0),
-    new Quote (5,'Charles','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2019,7,17),0,0),
-    new Quote (6,'Alfred','Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn',new Date(2019,7,15),0,0)
+    new Quote (1,'Tommy','Experience','What Does Not Kill you makes you Stronger.','gregory issacs', new Date(2022,2,12),0,0),
+    new Quote (2,'Mary Jane','mindsate…','Madness is like gravity all it needs is a little Push.','The joker',new Date(2022,2,14),0,0),
+    new Quote (3,'Mikey','The great myth','The great myth of our times is that technology is communication.','Libby Larsen',new Date(2022,1,3),0,0),
+    new Quote (4,'Beatrice','Communication','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2022,2,2),0,0),
+    new Quote (5,'Charles','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2022,7,17),0,0),
+    new Quote (6,'Alfred','Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.','Alice Kahn',new Date(2022,1,16),0,0)
   ];
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
