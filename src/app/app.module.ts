9efaecd55a9c00706c3ddae8769ccce8761308d3
timmy <timothy.ndung'u@student.moringaschool.com>
2022-02-16 09:54:08 +0300

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePipePipe } from './date-pipe.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { HighlightQouteDirective } from './highlight-qoute.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    DatePipePipe,
+    QuotesFormComponent,
+    HighlightQouteDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SweetAlert2Module.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
